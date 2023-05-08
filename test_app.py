3acd7874a5bb082e61d19019523ad53bd4383311
HieronimBajowski <106467648+HieronimBajowski@users.noreply.github.com>
2023-05-08 17:07:07 +0200

    zadanie
    First commit

---
+++ test_app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import unittest
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+    def test_app_runs(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+if __name__ == '__main__':
+    unittest.main()
